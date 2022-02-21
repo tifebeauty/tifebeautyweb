15bb7d7704a9af918ded4f5781214b3bbdc49ca1
Taiye <taiyeoguns@yahoo.com>
2022-02-21 21:59:09 +0000

    Initial commit

---
+++ vqmod/vqcache/vq2-system_library_template_twig.php
@@ -0,0 +1,45 @@
+<?php
+namespace Template;
+final class Twig {
+	private $data = array();
+
+	public function set($key, $value) {
+		$this->data[$key] = $value;
+	}
+	
+	public function render($filename, $code = '') {
+		if (!$code) {
+			$file = modification( DIR_TEMPLATE . $filename . '.twig' );
+            if( class_exists('VQMod') ) {
+                $file = \VQMod::modCheck($file, DIR_TEMPLATE . $filename . '.twig');
+            }
+            
+
+			if (is_file($file)) {
+				$code = file_get_contents($file);
+			} else {
+				throw new \Exception('Error: Could not load template ' . $file . '!');
+				exit();
+			}
+		}
+
+		// initialize Twig environment
+		$config = array(
+			'autoescape'  => false,
+			'debug'       => false,
+			'auto_reload' => true,
+			'cache'       => DIR_CACHE . 'template/'
+		);
+
+		try {
+			$loader = new \Twig\Loader\ArrayLoader(array($filename . '.twig' => $code));
+
+			$twig = new \Twig\Environment($loader, $config);
+
+			return $twig->render($filename . '.twig', $this->data);
+		} catch (Exception $e) {
+			trigger_error('Error: Could not load template ' . $filename . '!');
+			exit();
+		}	
+	}	
+}
