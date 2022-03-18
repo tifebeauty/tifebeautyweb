3a6a534911f24a2f40bb026382006100eed5d5a1
Taiye <taiyeoguns@yahoo.com>
2022-03-18 13:44:52 +0000

    hide ex tax price

---
+++ admin/language/en-gb/extension/module/hide_ex_tax.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title']     = 'Hide Ex Tax Price';
+
+// Text
+$_['text_extension']    = 'Extensions';
+$_['text_success']      = 'Success: You have modified Hide Ex Tax Price module!';
+$_['text_edit']         = 'Edit Hide Ex Tax Price Module';
+
+// Entry
+$_['entry_status']      = 'Status';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify Hide Ex Tax Price module!';
