a9916d98401b8e6dd5fa85e8ebb774f153ea534e
tifebeauty <100086774+tifebeauty@users.noreply.github.com>
2022-04-03 17:51:10 +0100

    multiple geo zone shipping (#6)
    Co-authored-by: Taiye <taiyeoguns@yahoo.com>

---
+++ admin/language/en-gb/extension/shipping/geo_zone_shipping.php
@@ -0,0 +1,63 @@
+<?php
+// Heading
+$_['heading_title']		= 'Geo Zone Shipping';
+
+// Text
+$_['text_edit']		   = 'Edit Geo Zone Shipping';
+$_['text_shipping']		= 'Shipping';
+$_['text_extension']		= 'Extension';
+$_['text_success']		= 'Success: You have modified Geo Zone Shipping Module!';
+$_['text_next']			= 'Next';
+$_['text_back']			= 'Back';
+$_['text_save']			= 'Save';
+
+// Tab
+$_['tab_general']			= 'General';
+$_['tab_methods']			= 'Methods';
+
+
+// Columns
+$_['column_general']		= 'General';
+$_['column_availability']= 'Availability';
+$_['column_cost']			= 'Shipping Cost';
+
+// Entry
+$_['entry_sort_order']	= 'Sort Order';
+$_['entry_method_name']	= 'Method Name';
+$_['entry_instruction']	= 'Shipping Instructions';
+$_['entry_status']		= 'Status';
+$_['entry_total']			= 'Total';
+$_['entry_geo_zone']		= 'Geo Zone';
+$_['entry_tax_class']	= 'Tax Class';
+$_['entry_no_tax_class']= '--- No Tax Class ---';
+$_['entry_cost_type']	= 'Cost Type';
+$_['entry_flat_cost']	= 'Flat Cost';
+$_['entry_price']			= 'Total ';
+$_['entry_price_cost']	= 'Total Based Cost';
+$_['entry_weight']		= 'Weight ';
+$_['entry_weight_cost']	= 'Weight Based Cost';
+
+
+// Button
+$_['button_add']			= 'Add Shipping Method';
+$_['button_remove']		= 'Remove Shipping Method';
+
+
+// Help
+$_['entry_total_help']		= 'Sub-Total amount needed before this shipping method becomes available. Leave empty or 0 to make it available for any amount.';
+$_['entry_flat_cost_help']		= 'Flat charges for shipping';
+
+$_['entry_price_help']		= 'Cart total needed before this shipping method becomes available. Leave empty or 0 to make it available for any cart total.';
+$_['entry_price_cost_help1']	= '50:10.00,70:12.00,100:15.00';
+$_['entry_price_cost_help2']	= 'Example 50:10.00,70:12.00,100:15.00 Which means upto cart total of 50 the shipping cost will be 10, from 50 to 70 the shipping cost will be 12 and for cart total over 100 the shipping cost will be 15';
+
+$_['entry_weight_help']		= 'Weight total needed before this shipping method becomes available. Leave empty or 0 to make it available for any weight.';
+$_['entry_weight_cost_help1']	= '5:10.00,7:12.00,10:15.00';
+$_['entry_weight_cost_help2']	= 'Example 5:10.00,7:12.00,10:15.00 Which means upto 5Kg the cost will be 10, from 5kg to 7kg the cost will be 12 and for over 10kg the cost will be 15';
+
+
+// Error
+$_['error_permission']	= 'Warning: You do not have permission to modify Geo Zone Shipping!';
+$_['error_method_name']	= 'Shipping Method Name cannot be empty!';
+$_['error_total']			= 'Total must be a numeric value';
+$_['error_flat_cost']	= 'Flat Cost must be a numeric value';
